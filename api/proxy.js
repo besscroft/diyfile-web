94c6c25d6886ae6a7ee52f68a03b6f237b943443
besscroft <besscroft@foxmail.com>
2022-12-18 22:03:46 +0800

    fix:proxy error

--- api/proxy.js
+++ api/proxy.js
@@ -3,14 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
 
-  if (req.url.startsWith('/@api')) {
-    target = 'https://api.besscroft.com/@api'
+  if (req.url.startsWith('/api')) {
+    target = 'https://api.besscroft.com/api'
   }
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/@api': '/',
+      '^/api/': '/',
     },
   })(req, res)
 }
--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,3 @@
 /** 全局配置 */
 // api 前缀
-export const API_URL = '/@api'
+export const API_URL = '/api'
