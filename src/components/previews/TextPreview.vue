40f651eff4990e258f83d2ddcae54eda6095454f
besscroft <besscroft@foxmail.com>
2023-03-05 15:17:12 +0800

    bug 修复

--- src/components/previews/TextPreview.vue
+++ src/components/previews/TextPreview.vue
@@ -42,7 +42,7 @@ onBeforeMount(() => {
     <button type="button" class="inline-block px-6 py-2 border-2 border-blue-400 text-blue-400 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" @click="copy(props.fileInfo.url)">
       <icon-copy /> {{ !copied ? t('button.copyUrl') : t('button.copyOk') }}
     </button>
-    <button v-if="props.storageInfo.type === 1" type="button" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" @click="handleDownload(props.fileInfo.proxyUrl)">
+    <button v-if="props.storageInfo.type === 1 && props.fileInfo.proxyUrl" type="button" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" @click="handleDownload(props.fileInfo.proxyUrl)">
       <icon-download /> {{ t('button.proxyDownload') }}
     </button>
     <button type="button" class="cursor-not-allowed inline-block px-6 py-2 border-2 border-gray-200 text-gray-200 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
