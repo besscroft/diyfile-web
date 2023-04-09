ec30a40ab4cd7813faf768809ca4566b478a180f
besscroft <besscroft@foxmail.com>
2023-04-09 18:51:01 +0800

    fix:依赖和样式调整

--- src/components/previews/VideoPreview.vue
+++ src/components/previews/VideoPreview.vue
@@ -54,20 +54,20 @@ onUnmounted(() => {
   <v-divider :thickness="2" class="border-opacity-50" color="success"></v-divider>
   <v-alert border="start" color="blue-lighten-4" :text="`正在预览：${decodeURIComponent(props.fileInfo.name)} 如果格式不支持，请切换浏览器或者编码器！`"></v-alert>
   <v-divider :thickness="2" class="border-opacity-50" color="success"></v-divider>
-  <v-alert class="flex flex-wrap justify-center items-center h-12" color="cyan-lighten-4">
-    <v-btn prepend-icon="download" class="mx-2" color="green-accent-3" @click="handleDownload(props.fileInfo.url)">
+  <div class="flex flex-wrap justify-center items-center space-x-2 min-h-12">
+    <v-btn prepend-icon="download" class="my-1" color="green-accent-3" @click="handleDownload(props.fileInfo.url)">
       {{ t('button.download') }}
     </v-btn>
-    <v-btn prepend-icon="content_copy" class="mx-2" color="teal-accent-1" @click="copy(props.fileInfo.url)">
+    <v-btn prepend-icon="content_copy" class="my-1" color="teal-accent-1" @click="copy(props.fileInfo.url)">
       {{ !copied ? t('button.copyUrl') : t('button.copyOk') }}
     </v-btn>
-    <v-btn v-if="props.storageInfo.type === 1 && props.fileInfo.proxyUrl" prepend-icon="download" class="mx-2" color="blue-grey-lighten-3" @click="handleDownload(props.fileInfo.proxyUrl)">
+    <v-btn v-if="props.storageInfo.type === 1 && props.fileInfo.proxyUrl" prepend-icon="download" class="my-1" color="blue-grey-lighten-3" @click="handleDownload(props.fileInfo.proxyUrl)">
       {{ t('button.proxyDownload') }}
     </v-btn>
-    <v-btn prepend-icon="sentiment_satisfied" variant="tonal" class="mx-2">
+    <v-btn prepend-icon="sentiment_satisfied" class="my-1" variant="tonal">
       其它操作开发中
     </v-btn>
-  </v-alert>
+  </div>
 </template>
 
 <style scoped>
