abf54117bff59fec1dd975cc1b37b455208fb992
besscroft <besscroft@foxmail.com>
2023-08-19 17:21:42 +0800

    chore:update deps

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ import { getFileNameFromPath } from '~/utils/FileUtil'
 import { getBaseUrl } from '~/utils/WindowUtil'
 
 /* global */
-console.log(`${'\n'} %c DiyFile v0.7.1 %c https://github.com/besscroft/diyfile ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;')
+console.log(`${'\n'} %c DiyFile v0.8.0 %c https://github.com/besscroft/diyfile ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;')
 
 generatedRoutes.push({
   path: '/',
